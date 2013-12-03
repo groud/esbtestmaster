9a127220419b04806586523cd26d68f39845f098
gilles <gilles@gilles-debian>
2013-12-03 09:55:31 +0100

    Creating 2 WS

---
+++ ESBTestCP/src/java/monitoring/MonitoringWS.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package monitoring;
+
+import javax.jws.WebService;
+
+/**
+ *
+ * @author gilles
+ */
+@WebService()
+public class MonitoringWS {
+
+}
---
+++ ESBTestCP/src/java/simulation/SimulationWS.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package simulation;
+
+import javax.jws.WebService;
+
+/**
+ *
+ * @author gilles
+ */
+@WebService()
+public class SimulationWS {
+
+}
