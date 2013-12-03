7dc59109930b4f5fd7937b7ac82b5f6bda60625a
gilles <gilles@gilles-debian>
2013-12-03 11:54:38 +0100

    Data structure modification

---
+++ ESBTestMaster/src/java/datas/ResultSet.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package datas;
+
+import java.util.SortedSet;
+
+/**
+ *
+ * @author gilles
+ */
+public class ResultSet {
+    private SortedSet<ResultEvent> events;
+}
