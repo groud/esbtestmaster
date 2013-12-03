d497c21f5b74de667f9442f2906cd2c59f0a347f
zachary <zachary@zacharies.(none)>
2013-12-03 10:27:30 +0100

    New interface included: ClientTerminal
    Committer: nickolay <zachary@zacharies.(none)>
    	new file:   ESBTestMaster/src/java/interfaces/ClientTerminal.java

---
+++ ESBTestMaster/src/java/interfaces/ClientTerminal.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package interfaces;
+
+/**
+ *
+ * @author zachary
+ */
+public interface ClientTerminal {
+    public int StartSimulation(String filename);
+    public int CalculateKPI();
+}
