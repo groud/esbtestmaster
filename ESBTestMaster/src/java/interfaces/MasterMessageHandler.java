d14741ea362f9642b09f48161b13ac46ac99d94b
Adrien <adrien.pompee@gmail.com>
2013-12-15 22:02:27 +0100

    Adding the JMS message handler on the master side

---
+++ ESBTestMaster/src/java/interfaces/MasterMessageHandler.java
@@ -0,0 +1,115 @@
+package interfaces;
+
+import java.io.Serializable;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author adrien
+ */
+
+  //generic JMS handler
+  //works with both queue and topic, the choice come from the object in JNDI
+
+public class MasterMessageHandler {
+
+    Context context = null;
+    ConnectionFactory factory = null;
+    Connection connection = null;
+    Destination toTopic = null;
+    Destination fromTopic = null;
+    Session session = null;
+    MessageProducer sender = null;
+    MessageConsumer receiver = null;
+
+    public MasterMessageHandler() {
+        try {
+            //To get to JNDI context
+            context = new InitialContext();
+            
+            //To get a ConnectionFactory from JNDI
+            factory = (ConnectionFactory) context.lookup("TopicConnectionFactory");
+
+            //Creating a connection from the factory
+            connection = factory.createConnection();
+
+            //Creating a session from the connection
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            
+            //Creating a producer to send config objects
+            toTopic = (Destination) context.lookup("config"); //Topic named "config"
+            sender = session.createProducer(toTopic);
+
+            //Creating a consumer to receive results objects
+            fromTopic = (Destination) context.lookup("results"); //Topic named "results"
+            receiver = session.createConsumer(fromTopic);
+
+            //Starting the connection
+            connection.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+            finalize();
+        }
+    }
+
+    public boolean sendToTopic(Serializable objectToSend) {
+        try {
+            final ObjectMessage message = session.createObjectMessage();
+            message.setObject(objectToSend);
+            sender.send(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+            finalize();
+        }
+        return true;
+    }
+
+    public Serializable receiveFromTopic() {
+        Serializable result = null;
+        try {
+            Message message = receiver.receive();
+            if (message instanceof ObjectMessage) {
+                ObjectMessage object = (ObjectMessage) message;
+                result = object.getObject();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            finalize();
+        }
+        return result;
+    }
+
+    @Override
+    protected void finalize() {
+        if (context != null) {
+            try {
+                context.close();
+            } catch (NamingException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (JMSException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
