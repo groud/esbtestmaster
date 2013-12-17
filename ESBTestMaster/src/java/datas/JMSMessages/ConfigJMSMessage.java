fa8f2ac7b88702d821b6d475ec4f64f9ac413289
zachary <zachary@zacharies.(none)>
2013-12-17 11:15:17 +0100

    @Master, JMS solution integration

---
+++ ESBTestMaster/src/java/datas/JMSMessages/ConfigJMSMessage.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package datas.JMSMessages;
+import datas.AgentConfiguration;
+import datas.SimulationScenario;
+import java.io.Serializable;
+
+/**
+ *
+ * @author BambaLamine
+ */
+public class ConfigJMSMessage implements Serializable{
+    private AgentConfiguration agent;
+    private SimulationScenario scenario;
+
+    public AgentConfiguration getAgentConfiguration(){
+        return this.agent;
+    }
+
+    public SimulationScenario getScenario(){
+        return this.scenario;
+    }
+    public void putAgentConfiguration(AgentConfiguration agent){
+        this.agent = agent;
+    }
+
+    public void putScenario(SimulationScenario scenario){
+        this.scenario = scenario;
+    }
+
+}
+
+
---
+++ ESBTestMaster/src/java/datas/JMSMessages/StopJMSMessage.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package datas.JMSMessages;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author BambaLamine
+ */
+public class StopJMSMessage implements Serializable{
+
+}
